34418ba681bc3d2aa91a2444c8c4422da5bb3374
st234pa <stephanie.yoon123@gmail.com>
2020-06-14 00:38:33 -0400

    login fetch token, update djangorest settings to allow ip as host

--- client/app/App.tsx
+++ client/app/App.tsx
@@ -5,9 +5,14 @@ import { AppLoading } from 'expo';
 import * as ScreenOrientation from 'expo-screen-orientation';
 import * as Font from 'expo-font';
 import { Ionicons } from '@expo/vector-icons';
-import { Button, Text } from 'native-base';
+import { Root, Button, Text } from 'native-base';
 import Home from './components/Home';
 import Login from './components/Login';
+
+export type RootStackParamList = {
+  Home: undefined;
+  Login: undefined;
+};
 
 export default function App() {
   const [isReady, setIsReady] = React.useState<boolean>(false);
@@ -23,32 +28,34 @@ export default function App() {
   if (!isReady) {
     return <AppLoading />;
   }
-  const Stack = createStackNavigator();
+  const Stack = createStackNavigator<RootStackParamList>();
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Login"
-          component={Login}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{
-            title: 'Hello',
-            headerRight: () => (
-              <Button
-                transparent
-                onPress={() => alert('This is a button!')}
-                color="#fff"
-              >
-                <Text>Header</Text>
-              </Button>
-            ),
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Root>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{
+              title: 'Hello',
+              headerRight: () => (
+                <Button
+                  transparent
+                  onPress={() => alert('This is a button!')}
+                  color="#fff"
+                >
+                  <Text>Header</Text>
+                </Button>
+              ),
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Root>
   );
 }
--- client/app/components/Login.tsx
+++ client/app/components/Login.tsx
@@ -1,5 +1,5 @@
-import React, { FunctionComponent } from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { FunctionComponent, createRef } from 'react';
+import { StyleSheet, View, Dimensions, TextInput } from 'react-native';
 import {
   Container,
   Text,
@@ -7,47 +7,111 @@ import {
   Form,
   Item,
   Input,
+  Icon,
   Button,
 } from 'native-base';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { RootStackParamList } from '../App';
+import { ip } from '../Secure';
 
-type LoginProps = {};
+type LoginProps = {
+  navigation: StackNavigationProp<RootStackParamList, 'Login'>;
+};
 
 const Login: FunctionComponent<LoginProps> = (props: LoginProps) => {
+  const [username, setUsername] = React.useState<string>('');
+  const [password, setPassword] = React.useState<string>('');
+  const [usernameError, setUsernameError] = React.useState<string>('');
+  const [passwordError, setPasswordError] = React.useState<string>('');
+
+  function loginRequest() {
+    fetch(`http://${ip}:8000/api/token`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username: username, password: password }),
+    })
+      .then((response: Response) => response.json())
+      .then((result: JSON) => {
+        console.log(result);
+      })
+      .catch((reason: any) => console.log(reason));
+  }
+
   return (
     <Container>
       <Content padder>
         <Form>
-          <View style={styles.titleView}>
-            <Text style={styles.title}>Reinvented Checkers</Text>
-          </View>
           <View style={styles.textInputsView}>
+            <View style={styles.titleView}>
+              <Text style={styles.title}>Reinvented Checkers</Text>
+            </View>
             <View style={styles.paddedItem}>
-              <Item rounded style={styles.item}>
+              <Item
+                error={usernameError.length > 0}
+                rounded
+                style={styles.item}
+              >
                 <Input
                   style={styles.input}
-                  placeholder="Username"
+                  placeholder={
+                    usernameError.length > 0 ? usernameError : 'Username'
+                  }
                   autoCapitalize="none"
+                  onChangeText={(text: string) => {
+                    setUsernameError('');
+                    setUsername(text);
+                  }}
+                  enablesReturnKeyAutomatically
+                  returnKeyType="done"
                 />
+                {usernameError.length > 0 && (
+                  <Icon style={styles.icon} name="close-circle" />
+                )}
               </Item>
             </View>
             <View style={styles.paddedItem}>
-              <Item rounded style={styles.item}>
+              <Item
+                error={passwordError.length > 0}
+                rounded
+                style={styles.item}
+              >
                 <Input
                   style={styles.input}
                   placeholder="Password"
                   autoCapitalize="none"
                   secureTextEntry={true}
+                  onChangeText={(text: string) => {
+                    setPassword(text);
+                  }}
+                  enablesReturnKeyAutomatically
+                  returnKeyType="done"
                 />
               </Item>
             </View>
           </View>
           <View style={styles.paddedItem}>
-            <Button rounded info block style={styles.item}>
+            <Button
+              rounded
+              info
+              block
+              style={styles.item}
+              onPress={() => {
+                loginRequest();
+              }}
+            >
               <Text style={styles.buttonText}>Login</Text>
             </Button>
           </View>
           <View style={styles.paddedItem}>
-            <Button rounded warning block style={styles.item}>
+            <Button
+              rounded
+              warning
+              block
+              style={styles.item}
+              onPress={() => props.navigation.navigate('Home')}
+            >
               <Text style={styles.buttonText}>Sign Up</Text>
             </Button>
           </View>
@@ -60,10 +124,11 @@ const Login: FunctionComponent<LoginProps> = (props: LoginProps) => {
 const styles = StyleSheet.create({
   titleView: { paddingVertical: 75 },
   title: { textAlign: 'center', fontSize: 30 },
-  textInputsView: { paddingBottom: 75 },
+  textInputsView: { height: Math.round(Dimensions.get('window').height) - 200 },
   item: { height: 55 },
   paddedItem: { paddingBottom: 20 },
   input: { fontSize: 16, paddingLeft: 20 },
+  icon: { paddingRight: 20 },
   buttonText: { fontSize: 16 },
 });
 
